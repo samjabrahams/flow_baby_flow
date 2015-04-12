c051ba9d735f9dea9a099a977031c6d6a4d4e3a0
Andrew Sutton <Andrew.J.Sutton@jpl.nasa.gov>
2015-04-11 22:47:05 -0700

    Water Data Extraction by parameter and country

--- py/USGSdata.py
+++ py/USGSdata.py
@@ -1,18 +1,33 @@
 import urllib2
 import pandas as pd
-
+import numpy as np
+import json
 import pdb
-
+import time
+import re
+import StringIO
+import matplotlib.pyplot as plt
 class USGSdata(object):
     '''
     A Class to interface to the USGS data bases via RESTlike interface. Data is output as a pandas data frame 
     '''
+    stat_cd = {'Maximum Values':"00001",
+                'Minimum Values':"00002",
+                'Mean Values':"00003",
+                'AM Values':"00004",
+                'PM Values':"00005",
+                'Summation Values':"00006",
+                'Modal Values':"00007",
+                'Median Values':"00008",
+                'Standard Deviation Values':"00009",
+                'Variance Values':"00010"}
     
     def __init__(self):
         ''' 
         Build the initial object for interfacing
         '''
         self.restPath  = "http://waterservices.usgs.gov/nwis/site/?"
+        self.dataPath = "http://waterservices.usgs.gov/nwis/dv/?"
         
         # Build list of counties
         data = pd.read_csv('fips_codes_website.csv',header=0)
@@ -42,14 +57,6 @@ class USGSdata(object):
                                                              row['County FIPS Code']))
         countryCodesDF = pd.DataFrame(countyCodes,columns=['USGS County Codes'])
         self.countyData = pd.concat([countyDataDF,countryCodesDF], join='inner', axis=1)
-            
-        
-    def makeUSGSCountyCode(self, county):   
-        ''' Generate the USGS county FIPS code including the State code based upon the county database'''
-        # Get the State
-        state = county['State Abbreviation']
-        stateCode = self.stateCodes 
-        return stateCode
         
     def getCountySites(self,countyIDs, format = 'rdb'):
         ''' Takes a county ID code a fetches information for the county's Sites.'''
@@ -63,7 +70,6 @@ class USGSdata(object):
         else:
             formatArg = "format=rdb"
 
-        
         # REST Argument
         arg = "&".join([countryArg,formatArg])
         RESTrequest = self.restPath + arg
@@ -72,18 +78,94 @@ class USGSdata(object):
         dataDF = pd.read_table(RESTrequest,
                             delimiter='\t',
                             comment  ='#')
-        dataDF
 
-    
-    
-    def getCountyData(countyID, codes = ''):
-        ''' Get the Water Data for the specified county'''
+    def getCountyStatisticsData(self,countyIDs, stat_cd = '00001', startDT='2010-11-22', endDT='2014-11-22'):
+        ''' 
+        Get the Water Data for the specified county as specified by the county's 5 digit FIPS ID. Statistics stat_cd codes are:
+            'Maximum Values':"00001",
+            'Minimum Values':"00002",
+            'Mean Values':"00003",
+            'AM Values':"00004",
+            'PM Values':"00005",
+            'Summation Values':"00006",
+            'Modal Values':"00007",
+            'Median Values':"00008",
+            'Standard Deviation Values':"00009",
+            'Variance Values':"00010"
         
+            Further documentation available at:     http://waterservices.usgs.gov/rest/DV-Service.html#format
+
+        '''
+        # Build the REST arguments
+        # County
+        countySTR = [str(cty) for cty in countyIDs]
+        countyArg = "countyCd="+','.join(countySTR)
+        # Format 
+        formatArg = "format=rdb"
+        # Codes (convert to String if in wrong format)
+        if isinstance(stat_cd, int):
+            stat_cd = "%05"%stat_cd
+        stat_cdArg= "statCd="+str(stat_cd)
+        # Parameters
+        parameterArgs = "parameterCd=00060"
+        # Dates
+        startDateArg = "startDT=" + startDT
+        endDateArg = "endDT=" + endDT
+        dateArg = "&".join([startDateArg,endDateArg])
+        # REST Argument
+        arg = "&".join([countyArg,formatArg,stat_cdArg,dateArg,parameterArgs])
+        RESTrequest = self.dataPath + arg
+        print RESTrequest
+        # Get data
+        try:
+            # Open the data url
+            urlData = urllib2.urlopen(RESTrequest)
+             # Create String buffer and parse the url
+            csvDataBuf = parse_USGS_csv(urlData)
         
-           
+            # Define Date parser function
+            def dateparser(dates):
+                return [time.strptime(date,format='%Y-%m-%d') for date in dates]
+                
+            # Make a data frame
+            DF = pd.read_table(StringIO.StringIO(csvDataBuf),
+                            comment = '#',
+                            header=None)
+            DF.columns = ['Agency','Site','Date','Value','Q']
+            DF['Value'] = DF[ 'Value'].astype('float')
+            DF['Date'] = pd.to_datetime(DF['Date'])
+            
+            # Collapse site data
+            data = DF[['Date','Value']].groupby('Date').sum()
+            return data
+        except:
+            pass
+
+def parse_USGS_csv(urlData):
+    csvData = ''
+    # Look for the header
+    for t in urlData:
+        m = re.search(r'^[#\da]', t)
+        # Get non comment lines
+        if (m == None):
+            if csvData == '':
+                csvData = t
+            else:
+                csvData = '\n'.join([csvData,t])
+    return csvData
     
 if __name__ == '__main__':
     
     waterData = USGSdata()
-    counties = [51059,51061]
-    waterData.getCountySites(counties, format='')
+    counties = waterData.countyData['USGS County Codes'].iloc[np.arange(0,1000,20)]
+    for county in counties:
+        try:
+            print county
+            data=waterData.getCountyStatisticsData([county],
+                            stat_cd='00001',
+                            startDT='2006-01-01',
+                            endDT='2014-01-01')
+            print 'County: %s, data: %d'%(county, len(data))
+        except:
+            pass
+        
