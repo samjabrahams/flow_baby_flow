f61f713b6ce88e0386a997e9a722a8d4b5254f6b
Joe <joerenzullo@gmail.com>
2015-04-12 16:16:09 -0700

    Added Parse script for JSON (has comments to explain what it is doing)

---
+++ parseGeoJSON.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Begin block happens once, middle block happens for each line in the file matching the pattern between //
+#END block happens once at the end
+#loop from 01 to 90 in increments of 1
+for c in {01..90..1}
+do
+
+awk 'BEGIN{printf "{\n\"type\": \"FeatureCollection\",\n\"features\": [" >> "'$c'.txt"}
+/{ "type":.*"STATE": "'$c'",.*/ {print $0 "\n," >> "'$c'.txt"}
+END{printf "\n]\n}" >> "'$c'.txt"}' countyGeo.json
+done
