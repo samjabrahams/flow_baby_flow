38ae4b608b314c560e49341a8c9278e4f12bc7b1
Sam Abrahams <sjabraha@gmail.com>
2015-05-04 20:33:23 -0700

    Fixes Day selector bug in date picker
    Now correctly lists all possible days.

--- js/functions.js
+++ js/functions.js
@@ -10,15 +10,15 @@
 
 		var yearWanted = 2011;
 		var monthWanted = "10";
-		var dayWanted = "06";
+		var dayWanted = "01";
 
 		var today=new Date();
 		var dayfield=document.getElementById(dayfield);
 		var monthfield=document.getElementById(monthfield);
 		var yearfield=document.getElementById(yearfield);
 		for (var i=0; i<31; i++)
-		dayfield.options[i]=new Option(i, i+1);
-		dayfield.options[today.getDate()]=new Option(dayWanted, dayWanted, true, true) ;//select today's day
+		dayfield.options[i]=new Option(i+1, i+1);
+		//ayfield.options[0]=new Option(dayWanted, dayWanted, true, true) ;//select today's day
 		for (var m=0; m<12; m++)
 		monthfield.options[m]=new Option(monthtext[m], monthtext[m]);
 		monthfield.options[monthWanted - 1]=new Option(monthWanted, monthWanted, true, true) ;//select today's month
